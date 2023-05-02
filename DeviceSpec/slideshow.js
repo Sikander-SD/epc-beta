bcacc1271f6bb6de6e08f5049ed44db48ad5ea4c
SikanderSD <78195281+Sikander-SD@users.noreply.github.com>
2023-05-02 17:35:59 +0530

    Add files via upload

--- DeviceSpec/slideshow.js
+++ DeviceSpec/slideshow.js
@@ -1,7 +1,8 @@
 // ----------------------------------------------- load slides
 
 // Dynamically create slides and add them to the slideshow container
-function renderSlides(container) {
+function renderSlides(container) {  
+  if (container){
     // set slides    
     slidelist.forEach((sld,i)=>{i+=1
         const slide = document.createElement('div');        
@@ -17,6 +18,8 @@ function renderSlides(container) {
         slide.innerHTML = `<div class="u-clearfix u-sheet u-sheet-1"></div>`;
         container.appendChild(slide);
     })
+  }else setTimeout(()=>{renderSlides(container),1000})
+  
 };//END: renderSlides()
 
 // Dynamically create slide dots and add them to the slideshow
