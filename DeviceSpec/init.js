7cf020324c9cd5473fc8e2ef9f2b8ec663b7ebf9
SikanderSD <78195281+Sikander-SD@users.noreply.github.com>
2023-05-02 20:38:40 +0530

    Add files via upload

--- DeviceSpec/init.js
+++ DeviceSpec/init.js
@@ -28,4 +28,8 @@ function init(){
         "youtube":null,
         "whatsapp":null
     }
+   Object.keys(social_links).forEach((k)=>{
+        if (social_links[k]) { $("a[title='"+k+"']").attr("href",social_links[k]) }
+        else { $("a[title='"+k+"']").attr("style","visibility:hidden;") }
+    });
 };
