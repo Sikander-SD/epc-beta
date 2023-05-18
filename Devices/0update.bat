fd77be1d49c1e6c66b191b8257967de31d1b9cab
SikanderSD <78195281+Sikander-SD@users.noreply.github.com>
2023-05-18 23:28:03 +0530

    Add files via upload

--- Devices/0update.bat
+++ Devices/0update.bat
@@ -3,14 +3,17 @@ setlocal enabledelayedexpansion
 
 set "namelist=namelist.txt"
 
+//create empty file
 if not exist "%namelist%" ( echo.> "%namelist%" )
 
+//rename json files
 for %%F in (*.json) do (
    set "filename=%%~nxF"
    set "newname=!filename: =_!"
    if not "!filename!"=="!newname!" ( ren "%%F" "!newname!" )
 )
 
+//add json filename to namelist.txt
 for %%F in (*.json) do (
    set "filename=%%~nF"
    echo !filename:_= ! >> %namelist%
