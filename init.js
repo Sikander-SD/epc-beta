40d4b60cc10e36938b5e418cc91405ed873b6c2d
SikanderSD <78195281+Sikander-SD@users.noreply.github.com>
2023-05-22 13:20:10 +0530

    Add files via upload

--- init.js
+++ init.js
@@ -21,7 +21,7 @@ function init(){
     renderProducts()
     // social media links
     const social_links = {
-        "facebook":null,
+        "facebook":"https://www.facebook.com/100087793316866/",
         "twitter":null,
         "instagram":"https://www.instagram.com/elite.phones.club",
         "linkedin":null,
